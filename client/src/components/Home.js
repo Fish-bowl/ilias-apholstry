bc7a323e9ea64e42c771146aea39ac88715e982f
Fish-bowl <symbolt.ap@gmail.com>
2018-11-15 15:57:42 -0700

    layout scaffold

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,28 +4,46 @@ import {
   Container,
   Segment,
   Grid,
+  Rail,
 } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
     return (
-      <Grid>
-        <Grid.Row columns='1' >
-          <Grid.Column>
-            <Header style={styles.dev.header} textAlign='center' as='h1' >this is logo</Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns='2' style={styles.dev.secondRow} >
-          <Container>
-            <Grid.Column width='6' >
-              <Segment inverted ><p>this is a bio</p></Segment>
+      <Container>
+        <Header 
+          style={styles.dev.header} 
+          textAlign='center' 
+          as='h1' 
+        >
+          this is logo
+        </Header>
+        <Rail style={styles.dev.leftRail} >
+          <Segment><p>these are your costumer reviews</p></Segment>
+        </Rail>
+        <Rail float='right' style={styles.dev.rightRail} >
+          <Segment><p>this is your bio</p></Segment>
+        </Rail>
+        <Grid>
+          <Grid.Row columns='1'>
+            <Grid.Column style={styles.dev.column} >
+              <Segment style={styles.dev.slideshow} inverted >
+                <p>this is a random image gallery</p>
+              </Segment>
             </Grid.Column>
-            <Grid.Column width='6' >
-              <Segment  inverted ><p>these are costumer testimonys</p></Segment>
-            </Grid.Column>
-          </Container>
-        </Grid.Row>
-      </Grid>
+          </Grid.Row>
+          <Grid.Row columns='2' style={styles.dev.twoBoxRow} >
+            <Container>
+              <Grid.Column width='6' >
+                <Segment inverted ><p>this is a bio</p></Segment>
+              </Grid.Column>
+              <Grid.Column width='6' >
+                <Segment  inverted ><p>these are costumer testimonys</p></Segment>
+              </Grid.Column>
+            </Container>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
@@ -34,14 +52,37 @@ const styles = {
   dev: {
     header: {
       border: 'red 3px solid',
+      height: '100px'
     },
-    secondRow: {
+    leftRail: {
+      zIndex: '1',
+      position: 'fixed',
+      top: '85%'
+    },
+    rightRail: {
+      zIndex: '1',
+      position: 'fixed',
+      top: '30%',
+      right: '60px',
+    },
+    column: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    row: {
+      border: 'red 3px solid',
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    twoBoxRow: {
       border: 'red 3px solid',
       display: 'flex',
       justifyContent: 'space-around'
     },
     slideshow: {
       border: 'red 3px solid',
+      height: '450px',
+      width: '66%',
     }
   }
   
