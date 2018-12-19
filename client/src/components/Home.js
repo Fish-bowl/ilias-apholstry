a9ca7c56f0cc072d4a7fbb015ddcb72279989aea
Fish-bowl <symbolt.ap@gmail.com>
2018-12-19 14:47:45 -0700

    style reset

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,52 +12,58 @@ import Feed from './CustomerFeed'
 class Home extends Component {
   render() {
     return (
-      <Container>
-        {/* <Rail style={styles.dev.leftRail} >
-          <Segment><p>these are your costumer reviews</p></Segment>
-        </Rail>
-        <Rail float='right' style={styles.dev.rightRail} >
-          <Segment><p>this is your bio</p></Segment>
-        </Rail> */}
-        <Grid>
-          <Grid.Row columns='3'>
-            <Grid.Column style={styles.dev.logo} >
-              <Logo />
-            </Grid.Column>
-            <Grid.Column style={styles.dev.column} >
-              <Segment style={styles.dev.slideshow} inverted >
-                <p>this is a random image gallery</p>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column style={styles.dev.column} >
-              <Segment style={styles.dev.slideshow} inverted >
-                <p>this is a random image gallery</p>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns='2' style={styles.dev.twoBoxRow} >
-            <Grid.Column width='4' >
-              <Segment compact inverted>
-                <p>this is a bio</p>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column width='4' >
-              <Segment style={styles.dev.feed} inverted>
-                <Feed />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row style={styles.dev.row} >
-            <Segment inverted>
-              <p>
-                In laboris aute exercitation sit Lorem. Nulla aute ea sit anim labore sint deserunt laboris. Sunt ea nisi est quis nulla minim nisi tempor fugiat nostrud et. Nisi et qui tempor ea irure. Esse laboris Lorem aliquip proident velit minim voluptate ullamco nulla.
-                \n
-                Pariatur commodo enim qui laborum irure esse exercitation pariatur laborum in velit ea consequat.
-              </p>
-            </Segment>
-          </Grid.Row>
-        </Grid>
-      </Container>
+      // <Container>
+      //   {/* <Rail style={styles.dev.leftRail} >
+      //     <Segment><p>these are your costumer reviews</p></Segment>
+      //   </Rail>
+      //   <Rail float='right' style={styles.dev.rightRail} >
+      //     <Segment><p>this is your bio</p></Segment>
+      //   </Rail> */}
+      //   <Grid>
+      //     <Grid.Row columns='3'>
+      //       <Grid.Column style={styles.dev.logo} >
+      //         <Logo />
+      //       </Grid.Column>
+      //       <Grid.Column style={styles.dev.column} >
+      //         <Segment style={styles.dev.slideshow} inverted >
+      //           <p>this is a random image gallery</p>
+      //         </Segment>
+      //       </Grid.Column>
+      //       <Grid.Column style={styles.dev.column} >
+      //         <Segment style={styles.dev.slideshow} inverted >
+      //           <p>this is a random image gallery</p>
+      //         </Segment>
+      //       </Grid.Column>
+      //     </Grid.Row>
+      //     <Grid.Row columns='2' style={styles.dev.twoBoxRow} >
+      //       <Grid.Column width='4' >
+      //         <Segment compact inverted>
+      //           <p>this is a bio</p>
+      //         </Segment>
+      //       </Grid.Column>
+      //       <Grid.Column width='4' >
+      //         <Segment style={styles.dev.feed} inverted>
+      //           <Feed />
+      //         </Segment>
+      //       </Grid.Column>
+      //     </Grid.Row>
+      //     <Grid.Row style={styles.dev.row} >
+      //       <Segment inverted>
+      //         <p>
+      //           In laboris aute exercitation sit Lorem. Nulla aute ea sit anim labore sint deserunt laboris. Sunt ea nisi est quis nulla minim nisi tempor fugiat nostrud et. Nisi et qui tempor ea irure. Esse laboris Lorem aliquip proident velit minim voluptate ullamco nulla.
+      //           \n
+      //           Pariatur commodo enim qui laborum irure esse exercitation pariatur laborum in velit ea consequat.
+      //         </p>
+      //       </Segment>
+      //     </Grid.Row>
+      //   </Grid>
+      // </Container>
+      <div>
+        <Logo />
+        <Container>
+          
+        </Container>
+      </div>
     );
   }
 }
